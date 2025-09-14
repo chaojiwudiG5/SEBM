68fdff5387f28984296134fed097b3c687e4a136
Deshperaydon <1279416099@qq.com>
2025-09-14 12:42:00 +0800

    test ci

--- src/test/java/group5/sebm/controllertest/UserControllerTest.java
+++ src/test/java/group5/sebm/controllertest/UserControllerTest.java
@@ -1,5 +1,7 @@
 package group5.sebm.controllertest;
 
-
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 public class UserControllerTest {
+
 }
