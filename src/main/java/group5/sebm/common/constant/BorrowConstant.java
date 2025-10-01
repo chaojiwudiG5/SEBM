ff2bc05fe9db32e20f6c444b96c21d1bea3b9213
布洛芬下蛋 <1144607840@qq.com>
2025-10-01 12:20:46 +0800

    modify(return):add geoFence

--- src/main/java/group5/sebm/common/constant/BorrowConstant.java
+++ src/main/java/group5/sebm/common/constant/BorrowConstant.java
@@ -9,7 +9,13 @@ public class BorrowConstant {
     public static final int MINUS = -1;
     public static final int PLUS = 1;
 
-    public static final double CENTER_LONGITUDE = 103.787042;
-    public static final double CENTER_LATITUDE = 1.278143;
+    public static final double CENTER_LONGITUDE_HOME = 103.787042;
+    public static final double CENTER_LATITUDE_HOME = 1.278143;
+
+    public static final double CENTER_LONGITUDE_SCHOOL = 103.776167;
+    public static final double CENTER_LATITUDE_SCHOOL = 1.292350;
+
     public static final double RADIUS = 20;
+
+
 }
