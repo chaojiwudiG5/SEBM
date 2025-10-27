6b85d70dbf7304936acd05039f81acf84bb268be
布洛芬下蛋 <1144607840@qq.com>
2025-10-27 14:23:20 +0800

    add new tests of Maintenance

--- src/test/java/group5/sebm/Maintenance/UserMaintenanceRecordPoTest.java
+++ src/test/java/group5/sebm/Maintenance/UserMaintenanceRecordPoTest.java
@@ -10,28 +10,47 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserMaintenanceRecordPoTest {
 
     @Test
-    void testGetterAndSetter() {
+    void testGetterSetterAndLombokMethods() {
         UserMaintenanceRecordPo po = new UserMaintenanceRecordPo();
+        Date now = new Date();
 
         po.setId(1L);
         po.setDeviceId(100L);
-        po.setUserId(10L);
+        po.setUserId(200L);
         po.setDescription("description");
         po.setImage("http://image.url");
-        po.setStatus(0);
+        po.setStatus(1);
         po.setIsDelete(0);
-        Date now = new Date();
         po.setCreateTime(now);
         po.setUpdateTime(now);
 
+        // getter
         assertEquals(1L, po.getId());
         assertEquals(100L, po.getDeviceId());
-        assertEquals(10L, po.getUserId());
+        assertEquals(200L, po.getUserId());
         assertEquals("description", po.getDescription());
         assertEquals("http://image.url", po.getImage());
-        assertEquals(0, po.getStatus());
+        assertEquals(1, po.getStatus());
         assertEquals(0, po.getIsDelete());
         assertEquals(now, po.getCreateTime());
         assertEquals(now, po.getUpdateTime());
+
+        // toString()
+        assertTrue(po.toString().contains("description"));
+
+        // equals & hashCode
+        UserMaintenanceRecordPo po2 = new UserMaintenanceRecordPo();
+        po2.setId(1L);
+        po2.setDeviceId(100L);
+        po2.setUserId(200L);
+        po2.setDescription("description");
+        po2.setImage("http://image.url");
+        po2.setStatus(1);
+        po2.setIsDelete(0);
+        po2.setCreateTime(now);
+        po2.setUpdateTime(now);
+
+        assertEquals(po, po2);
+        assertEquals(po.hashCode(), po2.hashCode());
     }
 }
